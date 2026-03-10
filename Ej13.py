4dfd529a28745618813c67a712feb0a39b21d542
Miguel Ángel Santana González <msangon@EEAE.LOCAL>
2026-03-10 13:38:51 +0100

    Agenda

--- Ej13.py
+++ Ej13.py
@@ -7,14 +7,41 @@
 # - Eliminar un contacto 
 # - Mostar todos los contactos (quien quiera sufrir, qu elos devuelva ordenados alfabéticamente).
 
-Agenda = {
-    "Nombre":{"Name": "Miguel","Telefono":"666999333","Dirección":"Rúa Montón, 2 - Ferrol"}
-}
+agenda = {
+    "1":{"Nombre":"miguel","Telefono":"666999333","Direccion":"Rúa Montón, 2 - Ferrol"}, 
+    "2":{"Nombre": "fina", "Telefono":"555333111", "Direccion":"Residencia Galatea - Ferrol"}
+    }
 
-print(f"Está usted enel programa AGENDA. Seleccione una opción: ")
-print(f"1. Añadir un contacto.")
-print(f"2. Buscar un contacto por el nombre")
-print(f"3. Mostrar todos los contactos ordenados alfabeticamente")
+while True:
+    print(f"Está usted enel programa AGENDA. Seleccione una opción: ")
+    print(f"1. Añadir un contacto.")
+    print(f"2. Buscar un contacto por el nombre")
+    print(f"3. Mostrar todos los contactos ordenados alfabeticamente")
+    print(f"4. Salir del programa")
+    opcion = int(input(""))
+
+    if opcion == 1:
+        print(f"Introduzca los datos de la persona que desea añadir: ")
+        Nombre= input("Introduzca el nombre del contacto: ").lower()
+        Telefono = input("Introduzca el telefono del usuario: ")
+        Direccion = input("Introduzca la direccion del contacto: ")
+        agenda[len(agenda)+1] =  { Nombre, Telefono, Direccion}
+
+    elif opcion == 2: 
+        contacto_buscar = input(f"Introduzca el nombre del contacto a buscar: ").lower()
+        for i in range agenda[len(agenda)]
+
+        print (agenda.get(contacto_buscar))
+
+    elif opcion == 3:
+        agenda_ordenada = sorted(agenda, key=lambda x: x)
+        for contacto in agenda_ordenada:
+            print(contacto)
+
+    else:
+        break
+
+
 
 
 
--- MongoDBLocal.py
+++ MongoDBLocal.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
+from pymongo.errors import PyMongoError
 clientelocal = MongoClient('mongodb://localhost:27017/')
 db = clientelocal['mibase']                
 collection = db['productos']
@@ -14,3 +15,75 @@ except ConnectionFailure:
     print(f"Error de conexión a MongodB")
 
 # con hashlib se puede hashear la contraseña y así es más dificil descifrarla 
+
+# Aquí vamos a crear una variable para introducir nuevos elementos en la base de dato
+
+''' class producto:
+
+    def __init__ (self, nombre, precio, stock):
+         self.name = nombre
+         self.precio = precio
+         self.stock = stock
+         
+    def cambiar_precio(self, nuevo_precio):
+         self.precio = nuevo_precio
+    
+producto1 = producto(producto[self.name ], producto[self.precio], producto[self.stock])''' 
+
+while True:
+
+        print("Seleccione una opcion: ")
+        print("1 - Para introducir artículos.")
+        print("2 - Para buscar un artículo")
+        print("3 - Para actualizar productos")
+        print("4 - Para eliminar un artículo")
+        print("5 - Para salir del menú.")
+        opcion = (int(input()))
+
+        if opcion == 1:
+            cantidad_productos = int(input("¿Cuantos productos quiere introducir?: "))
+
+            productos_insertar = []
+
+            for i in range (cantidad_productos):
+
+                nombre = input("Indroduzca el nombre del producto: ").lower()
+                precio = float(input("Introduzca el precio del produzco: "))
+                stock = int(input("Introduzca el stock de productos: "))
+
+                producto_nuevo = {"nombre": nombre, 
+                "precio": precio,
+                "stock": stock
+                }
+
+            productos_insertar.append(producto_nuevo)
+
+            db.productos.insert_many(productos_insertar)
+
+        elif opcion ==2: 
+            
+            articulo_buscar = input("Instroduzca el nombre del artículo: ").lower()
+            articulos = db.productos.find({"nombre": articulo_buscar})
+
+            for articulo in articulos: 
+                 print(articulo)
+
+        elif opcion == 3:
+            articulo = input("¿Que articulo quiere actualizar? ").lower()
+            precio = float(input("¿Que precio quiere introducir? "))
+            db.productos.update_one({"nombre":articulo},{"$set":{"precio":precio}})
+
+
+        elif opcion == 4:
+             articulo = input("¿Que articulo quiere elimnar? ").lower()
+
+             db.productos.delete_one({"nombre":articulo})
+
+
+        elif opcion == 5:
+             break
+        
+
+for producto in db.productos.find({},{"_id":0}):
+    print (producto)
+
