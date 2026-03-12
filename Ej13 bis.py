78f3de58512ed0e486528a5214214bde21a1e377
Miguel Ángel Santana González <msangon@EEAE.LOCAL>
2026-03-12 13:35:45 +0100

    Agenda con JSON

--- Ej13 bis.py
+++ Ej13 bis.py
@@ -1,4 +1,4 @@
-# Crea un progrmaa que gestione una agenda de contactos USANDO DICCIONARIO donde: 
+# Crea un programa que gestione una agenda de contactos USANDO DICCIONARIO donde: 
 #  - La clave es el nombre de la persona 
 #  - El valor es su número de teléfono (opción de meter otro diccionario con télefono y dirección).
 # El progrma debe permitir: 
@@ -6,10 +6,22 @@
 # - Buscar un contacto por nombre
 # - Eliminar un contacto 
 # - Mostar todos los contactos (quien quiera sufrir, qu elos devuelva ordenados alfabéticamente).
-import json 
+import json
 
-with open("Ej13_bis_Jason.json", "r") as archivo:
-        agenda = json.load(archivo)
+MiAgenda="Ej13_bis_Jason.json"
+
+def cargar_agenda():
+    with open(MiAgenda, "r") as agenda:
+        archivo = json.load(agenda)
+    return archivo
+
+def guardar_agenda(archivo):
+    with open(MiAgenda, "w") as agenda:
+        json.dump(archivo, agenda, indent=4)
+
+agenda = cargar_agenda()
+
+guardar_agenda(agenda)
 
 while True:
     print(f"Está usted en el programa AGENDA. Seleccione una opción: ")
@@ -17,7 +29,8 @@ while True:
     print(f"2. Buscar un contacto por el nombre")
     print(f"3. Mostrar todos los contactos ordenados alfabeticamente")
     print(f"4. Eliminar un contacto")
-    print(f"5. Salir del programa")
+    print(f"5. Modificar un conctacto")
+    print(f"6. Salir del programa")
     opcion = int(input(""))
 
     if opcion == 1:
@@ -25,34 +38,77 @@ while True:
         Nombre= input("Introduzca el nombre del contacto: ").lower()
         Telefono = input("Introduzca el telefono del usuario: ")
         Direccion = input("Introduzca la direccion del contacto: ")
-        agenda[len(agenda)+1] =  { Nombre, Telefono, Direccion}
+    
+        MiAgenda[len(MiAgenda)+1] =  {
+            "Nombre":Nombre,
+            "Telefono":Telefono,
+            "Direccion":Direccion
+            }
+        guardar_agenda(MiAgenda)
+        print("Contacto añadido correctamente")
 
     elif opcion == 2: 
         contacto_buscar = input(f"Introduzca el nombre del contacto a buscar: ").lower()
-        encontrado = False
-        for key in agenda:
-            if contacto_buscar == agenda[key]["Nombre"]:
-                print(agenda[key])
-                encontrado = True
-                break
-        if not encontrado:
+        if contacto_buscar in MiAgenda:
+            print(f"\nNombre:{contacto_buscar}")
+            print(f"\nTelefono: {MiAgenda[key][contacto_buscar]["Telefono"]}")
+            print(f"\nDireccion: {MiAgenda[key][contacto_buscar]["Direccion"]}")
+
+        else:
             print("Contacto no encontrado")
 
     elif opcion == 3:
-        agenda_ordenada = sorted(agenda.values(), key=lambda x: x["Nombre"])
-        for contacto in agenda_ordenada:
-          print(contacto)
+        if len(MiAgenda) == 0:
+            print("La agenda está vacía")
+        else:
+            print("\nContactos ordenados alfabeticamente:")
+            agenda_ordenada = sorted(agenda.values(), key=lambda x: x["Nombre"])
+            for contacto in agenda_ordenada:
+                print(f"Nombre: {contacto["Nombre"]}, Telefono: {contacto["Telefono"]}, Direccion: {contacto["Direccion"]}")
     
     elif opcion == 4: 
-        nombre = input("Introduzca el contacto a elimintar: ").lower()
+        nombre = input("Introduzca el contacto a eliminar: ").lower()
         try: 
-            del agenda[key]["Nombre"]
+            del MiAgenda[key]["Nombre"]
             print("Contacto eliminado")
         except: 
             print("El contacto no existe")
+        guardar_agenda(MiAgenda)
+    
+    elif opcion == 5: 
+        contacto_modificar = input("Introduzca el contacto a modificar: ").lower()
+        if contacto_modificar in MiAgenda:
+            print("Elija una opción")
+            input("1. Modificar el nombre")
+            input("2. Modificar el telefono")
+            input("3. Modificar la direccion")
+            input("4. Salir")
+            opcion = int(input(""))
+            if opcion == 1:
+                nuevo_nombre=input("Introduzca el nuevo nombre")
+                MiAgenda[contacto_modificar].update({Nombre:nuevo_nombre})
+                guardar_agenda(MiAgenda)
 
+            elif opcion == 2: 
+                nuevo_telefono=input("Introduzca el nuevo telefono")
+                MiAgenda[contacto_modificar].update({Telefono:nuevo_telefono})
+                guardar_agenda(MiAgenda)                
+            elif opcion == 3: 
+                nueva_direccion=input("Introduzca la nueva direccion")
+                MiAgenda[contacto_modificar].update({Direccion:nueva_direccion})
+                guardar_agenda(MiAgenda)
+            else:
+                break
+        else:
+            print("Contacto no encontrado")
+    
     else:
         break
+
+
+
+            
+
 
 
 
